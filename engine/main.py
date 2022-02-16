d5b5e5e53bcfba27728e81933f12a1a5cd8a8418
AL-CS <elanmusklover@gmail.com>
2022-02-15 21:28:25 -0500

    Game Loop and Class

---
+++ engine/main.py
@@ -0,0 +1,78 @@
+import pygame as pg
+
+class Game():
+    """
+    Main game class holding major functions and variables
+    """
+
+    def loadimage(self, image: str):
+        img = pg.image.load(image)
+        return img
+
+    def __init__(self):
+        self.framerate: int = None
+        self.name: str = None
+        self.dimensions: tuple = None
+        self.icon: pg.Surface = None
+        self._running = False
+        self._clock = pg.time.Clock()
+
+    def rungame(self):
+        """
+        Function to run game
+        """
+        if self._running is not True and self.framerate and self.name and self.dimensions and self.icon is not None:
+            pg.init()
+            pg.display.set_mode(self.dimensions)
+            pg.display.set_caption(self.name)
+            pg.display.set_icon(self.icon)
+            self._running = True
+            self._mainloop()
+
+            print("Game loop started.")
+        else:
+            print("You have not initialized the game correctly, or the game is already running!")
+
+    def get(self, var: str):
+        """
+        Gets and returns private vars
+        """
+        if var.lower() == "framerate":
+            return self.framerate
+        elif var.lower() == "name":
+            return self.name
+        elif var.lower() == "dimensions":
+            return self.dimensions
+        elif var.lower() == "running":
+            return self._running
+        elif var.lower() == "clock":
+            return self._clock
+        elif var.lower() == "game":
+            return self
+
+    def _mainloop(self):
+        """
+        Starts game main loop
+
+        Inits display
+        """
+        while self._running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self._running = False
+            pg.display.update()
+            self._clock.tick(self.framerate)
+
+def main():
+    game = Game()
+    game.dimensions = (1200, 700)
+    game.name = "PaticeSmash"
+    game.framerate = 60
+
+    img = game.loadimage("img.jpg")
+    game.icon = img
+
+    game.rungame()
+
+if __name__=="__main__":
+    main()
